354051a43e277943da3ec2d8615d8eb61b3af52e
McJackass <alexandru.stafie@gmail.com>
2022-11-04 17:50:56 +0900

    detele tsu folder, added qsub for tsubame run

--- 00_prod/restart.sh
+++ 00_prod/restart.sh
@@ -1,9 +1,9 @@
 cp input/fuel.inp fuel.inp
-rm wh_lfr.* wh_lfr_*
 rm -rf original_files 
 rm -rf wh_lfrsuffleNo*
-rm job.out
+rm wh_lfr.* wh_lfr_*
 rm sss2.out*
+rm job.out
 rm calc_time.out
 rm vol_chk
 rm core.*
