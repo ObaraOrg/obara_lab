6850e325d5f1003d90f15f8ddba7f3669f19113f
McJackass <alexandru.stafie@gmail.com>
2022-12-01 15:48:54 +0900

    moved files

---
+++ 05_productivity_scripts/03_tmux_resource monitor/resource_monitor.sh
@@ -0,0 +1,48 @@
+tmux new -s resource_viewer
+
+tmux split-window -v
+tmux resize-pane -U 20
+tmux send-keys 'rsh olds02.candle.nr.titech.ac.jp' Enter
+
+tmux split-window -v
+tmux resize-pane -U 13
+tmux send-keys 'rsh olds03.candle.nr.titech.ac.jp' Enter
+
+tmux split-window -v
+tmux resize-pane -U 6
+tmux send-keys 'rsh olds04.candle.nr.titech.ac.jp' Enter
+
+tmux split-window -v
+tmux send-keys 'rsh olds05.candle.nr.titech.ac.jp' Enter
+
+tmux split-window -h
+tmux send-keys 'rsh olds09.candle.nr.titech.ac.jp' Enter
+
+tmux select-pane -t 3
+tmux split-window -h
+tmux send-keys 'rsh olds08.candle.nr.titech.ac.jp' Enter
+
+tmux select-pane -t 2
+tmux split-window -h
+tmux send-keys 'rsh olds07.candle.nr.titech.ac.jp' Enter
+
+tmux select-pane -t 1
+tmux split-window -h
+tmux send-keys 'rsh olds06.candle.nr.titech.ac.jp' Enter
+
+tmux select-pane -t 0
+sleep 3
+tmux send-keys 'top ' '-c ' '-i ' Enter
+
+tmux split-window -h
+
+tmux send-keys \
+'echo -e "\n\nPanel arrangement\n|olds1| cmd |\n|-----|-----|\n\
+|  2  |  6  |\n|  3  |  7  |\n|  4  |  8  |\n|  5  |  9  |\n" ' Enter
+
+for (( i = 2; i < 10 ; i++ )); do
+	tmux select-pane -t $i
+	tmux send-keys 'top ' '-c ' '-i ' Enter
+done
+
+tmux select-pane -t 1
---
+++ documentation/cluster.md
@@ -0,0 +1,23 @@
+# Usefull commands
+
+### Resource usage
+
+[Ganglia Resource monitor](http://192.168.11.206/ganglia/?c=olds)
+
+top
+
+ps
+
+iostat
+
+vmstat
+
+### Changing loggin
+
+`rsh` - remote shell
+Use this one to login and work interactively on a certain node (olds01 - olds09)
+```sh
+# Example
+rsh olds01.candle.nr.titech.ac.jp
+```
+ 
