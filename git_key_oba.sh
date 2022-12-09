a944867fdbd72ef5fc92899481e1d1435c2c0601
McJackass <alexandru.stafie@gmail.com>
2022-12-09 18:16:48 +0900

    modified the sh scrips, because of merge

--- git_key_oba.sh
+++ git_key_oba.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -i
 eval "$(ssh-agent -s)"
-<<<<<<< HEAD
-ssh-add .ssh/oba_lab_cluster
-# source this file to make it work
-=======
 ssh-add ~/.ssh/oba_cluster_key
->>>>>>> refs/remotes/origin/main
--- git_key_tsu.sh
+++ git_key_tsu.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -i
 eval "$(ssh-agent -s)"
-<<<<<<< HEAD
 ssh-add ~/.ssh/git_key_tsubame
-=======
-ssh-add ~/.ssh/tsubame_git_key
->>>>>>> refs/remotes/origin/main
-# source this file to make it work
