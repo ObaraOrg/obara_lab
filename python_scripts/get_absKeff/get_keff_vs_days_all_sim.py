f965b299591d88f2a69d1c9444d687b4b7e64c7f
Stalex <alexandru.stafie@gmail.com>
2023-02-07 20:34:29 +0900

    Made beautiful and added some comments

--- python_scripts/get_absKeff/get_keff_vs_days_all_sim.py
+++ python_scripts/get_absKeff/get_keff_vs_days_all_sim.py
@@ -1,110 +1,82 @@
-import os
 import re
+import numpy as np
+import matplotlib.pyplot as plt
+import serpentTools as sp
+
 from pathlib import Path
 
-import matplotlib.pyplot as plt
-import numpy as np
-import serpentTools
+# Suppersing the ver and reading err outputed by serpentTools
+sp.settings.rc["serpentVersion"] = "2.1.32"
+sp.settings.rc["verbosity"] = "error"
 
-serpentTools.settings.rc["serpentVersion"] = "2.1.32"
-
-BASE_PATH = Path.cwd()
-
-
-def pathconstructor(index):
-    return BASE_PATH / f"03_220d_5_step_no_opti_tsu/wh_lfrsuffleNo{index}/wh_lfr_res.m"
-    # return BASE_PATH/f"wh_lfr_res.m"
+# Mod these to adapt for your base sss2 file name
+FILE_NAME = "wh_lfr"
 
 
-def big() -> None:
-    folders = [f for f in os.listdir(".") if os.path.isdir(f)]
-    print("Entire graph ploting")
+def plot_keff(num_of_keffs_to_cut: int = 0) -> None:
+    """Function to
+        - progress through the folders and find every _res.m file for data
+        -
 
-    for simulation_name in folders:
-        print("Processing : ", simulation_name)
-        list_of_files = sorted(Path(simulation_name).rglob("wh_lfr_res.m"))
-        # Probable ISSUE, it may pick up any left over res.m file in the simulation folder
+    Args:
+        num_of_keffs_to_cut (int, optional): _description_. Defaults to 0.
+    """
+    
+    # Pathlib builds the listing of subdirectories
+    folders = [x for x in Path(".").iterdir() if x.is_dir()]
+    print("Ploting :")
+
+    plt.figure(figsize=(12, 9))
+
+    for sim_folder in folders:
+        print("Processing : ", sim_folder)
+        list_of_files = sorted(Path(sim_folder).rglob(f"{FILE_NAME}_res.m"))
+
+        # NOTE: Probable ISSUE, may pick up any left over dep.m in the sim dir
         files_str = [str(file) for file in list_of_files]
         files_str.sort(key=lambda f: int(re.sub(r"\D", "", f)))
-        with serpentTools.settings.rc:
-            serpentTools.settings.rc["verbosity"] = "error"
-            files_read = [serpentTools.read(file_loc) for file_loc in files_str]
+
+        files_read = [sp.read(file_loc) for file_loc in files_str]
 
         keffs = np.concatenate(
             [reader.resdata["absKeff"][:, 0] for reader in files_read]
         )
-        base_time_scale = files_read[0].resdata["burnDays"][0:, 0]
+        time_arr = files_read[0].resdata["burnDays"][0:, 0]
+        single_time_arr = len(time_arr)
+
+        # This is where the code generates the X axis for EFPD
         one_period = np.max(files_read[0].resdata["burnDays"])
-        time_array = base_time_scale
-
-        time_array = np.concatenate(
-            [base_time_scale + idx * one_period for idx in range(0, len(files_read))]
+        time_arr = np.concatenate(
+            [time_arr + idx * one_period for idx in range(0, len(files_read))]
         )
 
-        plt.plot(time_array, keffs)
-        plt.legend(folders, loc="lower right")
-
-    plt.xlabel("EFPD", fontsize=12)
-    plt.ylabel("Keff", fontsize=12)
-    plt.grid()
-    plt.savefig("keff_vs_days.png", dpi=1200)
-    plt.show()
-    plt.clf()
-
-
-def small() -> None:
-    folders = [f for f in os.listdir(".") if os.path.isdir(f)]
-    print("Zoomed graph ploting (last 5 steps)")
-
-    for simulation_name in folders:
-        print("Processing : ", simulation_name)
-        list_of_files = sorted(Path(simulation_name).rglob("wh_lfr_res.m"))
-        # Probable ISSUE, it may pick up any left over res.m file in the simulation folder
-        files_str = [str(file) for file in list_of_files]
-        files_str.sort(key=lambda f: int(re.sub(r"\D", "", f)))
-        with serpentTools.settings.rc:
-            serpentTools.settings.rc["verbosity"] = "error"
-            files_read = [serpentTools.read(file_loc) for file_loc in files_str]
-
-        keffs = np.concatenate(
-            [reader.resdata["absKeff"][:, 0] for reader in files_read]
-        )
-        base_time_scale = files_read[0].resdata["burnDays"][0:, 0]
-        one_period = np.max(files_read[0].resdata["burnDays"])
-        time_array = base_time_scale
-
-        time_array = np.concatenate(
-            [base_time_scale + idx * one_period for idx in range(0, len(files_read))]
-        )
-
-        short_time_scale = len(base_time_scale) * 5
-        time_array = time_array[-short_time_scale:]
+        # Instruction to cut the time_array if num_of_keffs_to_cut is given
+        short_time_scale = single_time_arr * num_of_keffs_to_cut
+        time_arr = time_arr[-short_time_scale:]
         keffs = keffs[-short_time_scale:]
 
-        # Put a legend below current axis
-        plt.plot(time_array, keffs)
-        plt.legend(
-            folders,
-            loc="lower center",
-            bbox_to_anchor=(0.5, -0.3),
-            fancybox=True,
-            shadow=True,
-            ncol=3,
-            fontsize=8,
-        )
+        plt.plot(time_arr, keffs, label=sim_folder)
 
+    # plt.legend(
+    #     folders,
+    #     loc="lower center",
+    #     bbox_to_anchor=(0.5, -0.3),
+    #     fancybox=True,
+    #     shadow=True,
+    #     ncol=3,
+    #     fontsize=8,
+    plt.legend(loc="best")
     plt.xlabel("EFPD", fontsize=12)
     plt.ylabel("Keff", fontsize=12)
     plt.grid()
-    plt.savefig(
-        "keff_vs_days_zoom.png",
-        dpi=1200,
-        bbox_inches="tight",
-    )
-    plt.show()
-    plt.clf()
+    save_fig = f"Keff_vs_EFPD_from_step_{num_of_keffs_to_cut}.png"
+    plt.savefig(save_fig, dpi=70)
+
+    # Put a legend below current axis
+    plt.plot(time_arr, keffs)
 
 
 if __name__ == "__main__":
-    big()
-    small()
+    plot_keff()
+    plot_keff(2)
+    plt.show()
--- python_scripts/get_absKeff/get_keff_vs_step_all_sim.py
+++ python_scripts/get_absKeff/get_keff_vs_step_all_sim.py
@@ -1,55 +1,67 @@
 import re
-from pathlib import Path
-
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 import serpentTools as sp
 
-sp.settings.rc["serpentVersion"] = "2.1.32"
+from pathlib import Path
 
-BASE_PATH = Path.cwd()
+# Suppersing the ver and reading err outputed by serpentTools
+sp.settings.rc["serpentVersion"] = "2.1.32"
+sp.settings.rc["verbosity"] = "error"
+
+# Mod these to adapt for your base sss2 file name
+FILE_NAME = "wh_lfr"
 
 
 def plot_keff(num_of_keffs_to_cut: int = 0) -> None:
-    folders = [x for x in Path(".").iterdir() if x.is_dir()]
-    print("Entire graph ploting")
+    """Function to:
+        - progress through the folders and find every _res.m file for data
+        - picks up the name of simulation and uses it for the plt legend
 
+    NOTE: The script needs to be put outside the sim folder because of pathlib
+
+    Args:
+        num_of_keffs_to_cut (int, optional): _description_. Defaults to 0.
+    """
+
+    # Pathlib builds the listing of subdirectories
+    folders = [x for x in Path(".").iterdir() if x.is_dir()]
+    print("Ploting :")
+    
     plt.figure(figsize=(12, 9))
-    for simulation_name in folders:
-        print("Processing : ", simulation_name)
-        list_of_files = sorted(Path(simulation_name).rglob("wh_lfr_res.m"))
-        # Probable ISSUE, may pick up any left over dep.m in the sim dir
+
+    for sim_folder in folders:
+        print("Processing : ", sim_folder)
+        list_of_files = sorted(Path(sim_folder).rglob(f"{FILE_NAME}_res.m"))
+
+        # NOTE: Probable ISSUE, may pick up any left over dep.m in the sim dir
         files_str = [str(file) for file in list_of_files]
         files_str.sort(key=lambda f: int(re.sub(r"\D", "", f)))
-        with sp.settings.rc:
-            sp.settings.rc["verbosity"] = "error"
-            files_read = [sp.read(file_loc) for file_loc in files_str]
+        files_read = [sp.read(file_loc) for file_loc in files_str]
 
         keffs = np.concatenate(
             [reader.resdata["absKeff"][:, 0] for reader in files_read]
         )
-        time_array = files_read[0].resdata["burnDays"][:, 0]
+        time_arr = files_read[0].resdata["burnDays"][:, 0]
+        single_time_arr = len(time_arr)
 
-        base_time = len(time_array)
+        # This is where the code generates the X axis for the steps
+        for index in range(len(time_arr)):
+            time_arr[index] = index / (len(time_arr) - 1)
+        time_arr = np.concatenate(
+            [time_arr + idx for idx in range(0, len(files_read))])
 
-        for index in range(len(time_array)):
-            time_array[index] = index / (len(time_array) - 1)
-
-        time_array = np.concatenate(
-            [time_array + idx for idx in range(0, len(files_read))]
-        )
-
-        short_time_scale = base_time * num_of_keffs_to_cut
-        time_array = time_array[-short_time_scale:]
+        short_time_scale = single_time_arr * num_of_keffs_to_cut
+        time_arr = time_arr[-short_time_scale:]
         keffs = keffs[-short_time_scale:]
 
-        plt.plot(time_array, keffs, label=simulation_name)
+        plt.plot(time_arr, keffs, label=sim_folder)
 
     plt.legend(loc="best")
     plt.xlabel("Shuffling step", fontsize=12)
     plt.ylabel("Keff", fontsize=12)
     plt.grid()
-    save_fig = f"keff_vs_days_starting_from_idx_{num_of_keffs_to_cut}.png"
+    save_fig = f"Keff_vs_STEPS_from_step_{num_of_keffs_to_cut}.png"
     plt.savefig(save_fig, dpi=70)
 
 
