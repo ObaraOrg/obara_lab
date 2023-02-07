1947a32afa9ca7430bf6b93297c01f7cdc1bce75
Petar Mirceski <petar.mirceski1998@gmail.com>
2023-02-07 23:18:54 +0900

    added the plotting scripts and added some matplotlib scale aligner function changed the requirements to use pylint

--- python_scripts/neutron_ballance/nb_keff_vs_burnup.py
+++ python_scripts/neutron_ballance/nb_keff_vs_burnup.py
@@ -1,54 +1,79 @@
 import copy
 from pathlib import Path
-from typing import List
 
 import matplotlib.pyplot as plt
 import numpy as np
 import serpentTools as sp
-from serpentTools.parsers.results import ResultsReader
+
+from mpl_axis_aligner.align import yaxes
 
 # Suppersing the ver and reading err outputed by serpentTools
 sp.settings.rc["serpentVersion"] = "2.1.32"
 sp.settings.rc["verbosity"] = "error"
 
 # Mod these to adapt for your base sss2 file name
-BASE_PATH = Path.cwd()
-FILE_NAME = "wh_lfr"
 
 # get rid of it and use pathlib...
-def pathconstructor(index):
-    return BASE_PATH / f"wh_lfr_res.m"
+FILE_NAME = "wh_lfr_fa"
+
+BURNUP = "burnup"
+BURN_DAYS = "burnDays"
+ABS_KINF = "absKinf"
+NUBAR = "nubar"
+FIMA = "fima"
+BREED_RATIO = "conversionRatio"
+
+
+def plot_twinx(
+    x_axis_data: np.ndarray,
+    y1_axis_data: np.ndarray,
+    y2_axis_data: np.ndarray,
+    y1_label: str = "",
+    y2_label: str = "",
+    x_label: str = "",
+) -> None:
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.plot(x_axis_data, y1_axis_data, c="green")
+    ax2.plot(x_axis_data, y2_axis_data, c="red")
+
+    yaxes(ax1, 0, ax2, 1, 0.75)
+    ax1.set_ylabel(y1_label)
+    ax2.set_ylabel(y2_label)
+    ax1.set_xlabel(x_label)
+    ax1.margins(x=0)
+    ax1.grid()
+
+
+def align_yaxis(ax1, v1, ax2, v2):
+    """adjust ax2 ylimit so that v2 in ax2 is aligned to v1 in ax1"""
+    _, y1 = ax1.transData.transform((0, v1))
+    _, y2 = ax2.transData.transform((0, v2))
+    inv = ax2.transData.inverted()
+    _, dy = inv.transform((0, 0)) - inv.transform((0, y1 - y2))
+    miny, maxy = ax2.get_ylim()
+    ax2.set_ylim(miny + dy, maxy + dy)
+
 
 def main() -> None:
 
-    res_list = []
-    for index in range(20):
-        res = sp.read(str(pathconstructor(index)))
-        res_list.append(res)
+    res_file = sp.read(f"{FILE_NAME}_res.m")
 
-    res_total = []
-    for res in res_list:
-        res_total.append(res.resdata)
+    TRIM_STEP = 15
+    burnup = res_file.resdata[BURNUP][:TRIM_STEP, 0]
+    burn_days = res_file.resdata[BURN_DAYS][:TRIM_STEP, 0]
+    kinf = res_file.resdata[ABS_KINF][:TRIM_STEP, 0]
+    numbar = res_file.resdata[NUBAR][:TRIM_STEP, 0]
+    fima = res_file.resdata[FIMA][:TRIM_STEP, 0]
+    # breed_ratio = res_file.resdata[BREED_RATIO][:TRIM_STEP, 0]
 
-    res_total_complete = copy.deepcopy(res_total[0])
-    for index in range(1, len(res_total)):
-        for key in res_total[0].keys():
-            if key == "burnup":
-                res_bu_sum = res_total[index][key] + res_total_complete[key][-1]
-                # Check the BU array for the second coll
-                res_total_complete[key] = np.vstack(
-                    (res_total_complete[key], res_bu_sum)
-                )
-            else:
-                res_total_complete[key] = np.vstack(
-                    (res_total_complete[key], res_total[index][key])
-                )
+    dbu = np.diff(burnup, prepend=burnup[0])
+    integral = np.cumsum(numbar * (1 - (1 / kinf)) * dbu)
 
-    x = res_total_complete["burnup"][:, 0]
-    y = res_total_complete["absKeff"][:, 0]
-    plt.plot(y)
-    plt.xticks(np.arange(x.shape[0]), [f"{el:.2f}" for el in x], rotation="vertical")
-    plt.locator_params(axis="x", nbins=26)
+    plot_twinx(burnup, integral, kinf, "NB", "$K_{inf}$", "burnup")
+    plt.show()
+
+    plot_twinx(fima, integral, kinf, "NB", "$K_{inf}$", "fima")
     plt.show()
 
 
