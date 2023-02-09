a2bb2c21d021e57dd2df74532e0c1c3bce3f3d5a
Stalex <alexandru.stafie@gmail.com>
2023-02-09 19:39:00 +0900

    Finished the NB script

--- python_scripts/neutron_ballance/nb_keff_vs_burnup.py
+++ python_scripts/neutron_ballance/nb_keff_vs_burnup.py
@@ -32,17 +32,25 @@ def plot_twinx(
 ) -> None:
 
     fig, ax1 = plt.subplots(figsize=(16, 9))
+
+    F_SIZE = 14
     ax2 = ax1.twinx()
-    ax1.plot(x_axis_data, y1_axis_data, c="green")
-    ax2.plot(x_axis_data, y2_axis_data, c="red")
+    ax1.plot(x_axis_data, y1_axis_data, c="green", marker="x", label=y1_label)
+    ax2.plot(x_axis_data, y2_axis_data, c="red", marker="x", label=y2_label)
+    start, end = tuple(round(x / 10) * 10 for x in ax1.get_xlim())
+    ax1.xaxis.set_ticks(np.arange(start, end + 1, end // (len(x_axis_data) // 2.5)))
+    ax1.legend(loc="upper left", fontsize=F_SIZE)
+    ax2.legend(loc="upper right", fontsize=F_SIZE)
 
     yaxes(ax1, 0, ax2, 1, 0.75)
-    ax1.set_ylabel(y1_label)
-    ax2.set_ylabel(y2_label)
-    ax1.set_xlabel(x_label)
+    ax1.set_title(f"{y1_label} and {y2_label} vs {x_label}", fontsize=F_SIZE)
+    ax1.set_ylabel(y1_label, fontsize=F_SIZE)
+    ax2.set_ylabel(y2_label, fontsize=F_SIZE)
+    ax1.set_xlabel(x_label, fontsize=F_SIZE)
     ax1.margins(x=0)
+    ax1.axhline(y=0, color="black", linestyle="--", linewidth=1)
     ax1.grid()
-
+    
 
 def align_yaxis(ax1, v1, ax2, v2):
     """adjust ax2 ylimit so that v2 in ax2 is aligned to v1 in ax1"""
@@ -54,42 +62,29 @@ def align_yaxis(ax1, v1, ax2, v2):
     ax2.set_ylim(miny + dy, maxy + dy)
 
 
-def main() -> None:
+def main(trim: int = -1) -> None:
 
     res_file = sp.read(f"{FILE_NAME}_res.m")
 
-    TRIM_DATA = 70
-    burnup = res_file.resdata[BURNUP][:TRIM_DATA, 0]
-    burn_days = res_file.resdata[BURN_DAYS][:TRIM_DATA, 0]
-    kinf = res_file.resdata[ABS_KINF][:TRIM_DATA, 0]
-    numbar = res_file.resdata[NUBAR][:TRIM_DATA, 0]
-    fima = res_file.resdata[FIMA][:TRIM_DATA, 0]
-
-    # burnup = res_file.resdata[BURNUP][:, 0]
-    # burn_days = res_file.resdata[BURN_DAYS][:, 0]
-    # kinf = res_file.resdata[ABS_KINF][:, 0]
-    # numbar = res_file.resdata[NUBAR][:, 0]
-    # fima = res_file.resdata[FIMA][:, 0]
-
-    # breed_ratio = res_file.resdata[BREED_RATIO][:TRIM_DATA, 0]
+    burnup = res_file.resdata[BURNUP][:trim, 0]
+    burn_days = res_file.resdata[BURN_DAYS][:trim, 0]
+    kinf = res_file.resdata[ABS_KINF][:trim, 0]
+    numbar = res_file.resdata[NUBAR][:trim, 0]
+    fima = res_file.resdata[FIMA][:trim, 0]
+    # breed_ratio = res_file.resdata[BREED_RATIO][:trim, 0]
 
     integral = cumulative_trapezoid(numbar * (1 - (1 / kinf)), burnup, initial=0)
 
-    plot_twinx(burnup, integral, kinf, "NB", "$K_{inf}$", "MWd/kgU")
-    save_fig = "NB_vs_BUdays.png"
-    plt.savefig(save_fig, dpi=70)
-    plt.show()
+    plot_twinx(burnup, integral, kinf, "NB", "$K_{inf}$", "Burnup $(\\frac{MWd}{kgU})$")
+    plt.savefig("NB_vs_BU.png", dpi=70)
 
-    plot_twinx(burn_days, integral, kinf, "NB", "$K_{inf}$", "EFPD")
-    save_fig = f"NB_vs_BUdays.png"
-    plt.savefig(save_fig, dpi=70)
-    plt.show()
+    plot_twinx(burn_days, integral, kinf, "NB", "$K_{inf}$", "Burnup (EFPD)")
+    plt.savefig("NB_vs_BUdays.png", dpi=70)
 
-    plot_twinx(fima * 100, integral, kinf, "NB", "$K_{inf}$", "%FIMA")
-    save_fig = f"NB_vs_FI.png"
-    plt.savefig(save_fig, dpi=70)
+    plot_twinx(fima * 100, integral, kinf, "NB", "$K_{inf}$", "[%] FIMA")
+    plt.savefig("NB_vs_FI.png", dpi=70)
     plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    main(70)
