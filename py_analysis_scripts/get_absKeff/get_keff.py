4d7105d71b3819d5e3fef0793e1cca6038ac0c7d
Alexandru Stafie <alexandru.stafie@gmail.com>
2023-06-07 17:54:39 +0900

    Finished the get_keff script, added to dataframe and saving features to excell and csv

--- py_analysis_scripts/get_absKeff/get_keff.py
+++ py_analysis_scripts/get_absKeff/get_keff.py
@@ -3,21 +3,25 @@ import copy
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
 import serpentTools as sp
 
-# Suppersing the ver and reading err outputed by serpentTools
+# Set display options to show all rows and columns
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+
+# Suppressing the ver and reading err outputed by serpentTools
 sp.settings.rc["serpentVersion"] = "2.1.32"
 sp.settings.rc["verbosity"] = "error"
 
 BASE_PATH = Path.cwd()
 
-# Mod these to adapt for your base sss2 file name
+# Modify these to adapt for your base sss2 file name
 FILE_NAME = "wh_lfr"
 PARAM = "absKeff"
 
 
 def get_keff_simple(to_print: str) -> None:
-
     # Get the file paths and sort them
     # NOTE: Probable ISSUE, may pick up any left over dep.m in the sim dir
     list_of_files = sorted(Path(BASE_PATH).rglob(f"{FILE_NAME}_res.m"))
@@ -27,12 +31,16 @@ def get_keff_simple(to_print: str) -> None:
     # Simple appending of any key inside the resdata
 
     res_list = []
-    for file in files_str:
-        res_list.append(sp.read(file))
+    fld_names = []  # Array to store the folder names
+    for file in list_of_files:
+        res_list.append(sp.read(str(file)))
+        fld_names.append(file.parent.name)
 
     res_total = []
+    fld_names_tot = []  # Array to store the folder names for each vstack
     for res in res_list:
         res_total.append(res.resdata)
+        fld_names_tot.extend([fld_names.pop(0)] * len(res.resdata[to_print]))
 
     res_total_complete = copy.deepcopy(res_total[0])
     for index in range(1, len(res_total)):
@@ -43,8 +51,29 @@ def get_keff_simple(to_print: str) -> None:
                     (res_total_complete[key], res_bu_sum)
                 )
 
-    print(res_total_complete[to_print])
+    # print(res_total_complete[to_print])
+
+    df = pd.DataFrame(
+        {
+            "K-eff": res_total_complete[to_print][:, 0],
+            "error": res_total_complete[to_print][:, 1],
+            "Folder Name": fld_names_tot,
+        }
+    )
+
+    # Extract the last number from the Folder Name
+    df["Step"] = df["Folder Name"].apply(
+        lambda x: int(re.findall(r"\d+$", x)[0])
+    )
+
+    # Reorder the columns
+    df = df.reindex(columns=["Folder Name", "Step", "K-eff", "error"])
+
+    return df
 
 
 if __name__ == "__main__":
-    get_keff_simple(PARAM)
+    df = get_keff_simple(PARAM)
+    print(df)
+    df.to_excel(f"{BASE_PATH}/K-effs.xlsx", index=False)
+    df.to_csv(f"{BASE_PATH}/K-effs.csv", index=False)
