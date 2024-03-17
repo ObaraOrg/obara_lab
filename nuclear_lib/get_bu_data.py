d18084ba1cbd03cc49b39bb3dfe6623a7697c287
Stalex <alexandru.stafie@gmail.com>
2024-03-18 07:23:53 +0900

    no functional modifications, commented the old function

--- nuclear_lib/get_bu_data.py
+++ nuclear_lib/get_bu_data.py
@@ -5,103 +5,103 @@ from nuclear_lib.split_fuel_names import split_pz_name
 from typing import Tuple, Dict
 
 
-def get_bu_data_old(
-    dep: DepletionReader, atomic_wt: pd.DataFrame, P: int, Z: int
-) -> Tuple[pd.DataFrame, np.ndarray, Dict[str, float], Dict[str, float]]:
-    """
-    Retrieves depletion data for different materials.
-    Retrieves nuclide desities from the dep.m file.
+# def get_bu_data_old(
+#     dep: DepletionReader, atomic_wt: pd.DataFrame, P: int, Z: int
+# ) -> Tuple[pd.DataFrame, np.ndarray, Dict[str, float], Dict[str, float]]:
+#     """
+#     Retrieves depletion data for different materials.
+#     Retrieves nuclide desities from the dep.m file.
 
-    Args:
-        dep (DepletionReader): Object containing dep.m file.
-        atomic_wt (pd.DataFrame): DataFrame with atomic weights of elements.
-        P (int): Number of fuel assemblies.
-        Z (int): Number of axial slices.
+#     Args:
+#         dep (DepletionReader): Object containing dep.m file.
+#         atomic_wt (pd.DataFrame): DataFrame with atomic weights of elements.
+#         P (int): Number of fuel assemblies.
+#         Z (int): Number of axial slices.
 
-    Returns:
-        Tuple[pd.DataFrame, np.ndarray, Dict[str, float], Dict[str, float]]: A tuple
-        containing the sorted DataFrame, burnup matrix, average burnups by pin,
-        and sum of burnups for each assembly.
+#     Returns:
+#         Tuple[pd.DataFrame, np.ndarray, Dict[str, float], Dict[str, float]]: A tuple
+#         containing the sorted DataFrame, burnup matrix, average burnups by pin,
+#         and sum of burnups for each assembly.
 
-    """
-    ordered_names = dep.names
-    element_names = set(atomic_wt.element)
+#     """
+#     ordered_names = dep.names
+#     element_names = set(atomic_wt.element)
 
-    # Dictionary comprehension and set checker
-    # Map element names to their indices in the ordered_names list
-    element_index_mapper = {
-        ordered_name: i
-        for i, ordered_name in enumerate(ordered_names)
-        if ordered_name in element_names
-    }
-    used_indxs = [index for _, index in element_index_mapper.items()]
-    indxs = np.array(used_indxs)
+#     # Dictionary comprehension and set checker
+#     # Map element names to their indices in the ordered_names list
+#     element_index_mapper = {
+#         ordered_name: i
+#         for i, ordered_name in enumerate(ordered_names)
+#         if ordered_name in element_names
+#     }
+#     used_indxs = [index for _, index in element_index_mapper.items()]
+#     indxs = np.array(used_indxs)
 
-    materials = dep.materials.keys()
-    # create my own dict with fuel vol's
-    fuel_vol_dict = {}
+#     materials = dep.materials.keys()
+#     # create my own dict with fuel vol's
+#     fuel_vol_dict = {}
 
-    # Go through each material index
-    for fuel_vol in materials:
-        if fuel_vol == "total":
-            continue
-        fuel_vol_dict[fuel_vol] = {
-            "mdens_sum": dep.materials[fuel_vol].mdens[:, -1][indxs].sum(),
-            "serpent_burnup": dep.materials[fuel_vol].burnup[-1],
-        }
+#     # Go through each material index
+#     for fuel_vol in materials:
+#         if fuel_vol == "total":
+#             continue
+#         fuel_vol_dict[fuel_vol] = {
+#             "mdens_sum": dep.materials[fuel_vol].mdens[:, -1][indxs].sum(),
+#             "serpent_burnup": dep.materials[fuel_vol].burnup[-1],
+#         }
 
-    for fuel_vol in fuel_vol_dict:
-        p_index, z_index = split_pz_name(str(fuel_vol))
-        fuel_vol_dict[fuel_vol]["p"] = p_index
-        fuel_vol_dict[fuel_vol]["z"] = z_index
-        mdens_to_divide_key = f"fuelP1Z{z_index}"
-        mdens_to_divide = dep.materials[mdens_to_divide_key].mdens[:, -1][indxs].sum()
-        ratio = fuel_vol_dict[fuel_vol]["mdens_sum"] / mdens_to_divide
-        fuel_vol_dict[fuel_vol]["mdens_ratio"] = ratio
+#     for fuel_vol in fuel_vol_dict:
+#         p_index, z_index = split_pz_name(str(fuel_vol))
+#         fuel_vol_dict[fuel_vol]["p"] = p_index
+#         fuel_vol_dict[fuel_vol]["z"] = z_index
+#         mdens_to_divide_key = f"fuelP1Z{z_index}"
+#         mdens_to_divide = dep.materials[mdens_to_divide_key].mdens[:, -1][indxs].sum()
+#         ratio = fuel_vol_dict[fuel_vol]["mdens_sum"] / mdens_to_divide
+#         fuel_vol_dict[fuel_vol]["mdens_ratio"] = ratio
 
-    for fuel_vol in fuel_vol_dict.keys():
-        p_index, z_index = split_pz_name(fuel_vol)
-        burn_up = fuel_vol_dict[fuel_vol]["serpent_burnup"]
-        if p_index == 1:
-            fuel_vol_dict[fuel_vol]["corrected_burnup"] = burn_up
-            continue
+#     for fuel_vol in fuel_vol_dict.keys():
+#         p_index, z_index = split_pz_name(fuel_vol)
+#         burn_up = fuel_vol_dict[fuel_vol]["serpent_burnup"]
+#         if p_index == 1:
+#             fuel_vol_dict[fuel_vol]["corrected_burnup"] = burn_up
+#             continue
 
-        material_name = f"fuelP{p_index - 1}Z{z_index}"
-        ratio = fuel_vol_dict[material_name]["mdens_ratio"]
-        corrected_burnup = burn_up * ratio
-        fuel_vol_dict[fuel_vol]["corrected_burnup"] = corrected_burnup
+#         material_name = f"fuelP{p_index - 1}Z{z_index}"
+#         ratio = fuel_vol_dict[material_name]["mdens_ratio"]
+#         corrected_burnup = burn_up * ratio
+#         fuel_vol_dict[fuel_vol]["corrected_burnup"] = corrected_burnup
 
-    average_burnups = {}
-    for fuel_vol in fuel_vol_dict.keys():
-        p_index, _ = split_pz_name(fuel_vol)
-        average_burnup = 0
-        for zidx in range(1, Z + 1):
-            key_construct = f"fuelP{p_index}Z{zidx}"
-            average_burnup += fuel_vol_dict[key_construct]["corrected_burnup"] / Z
-        average_burnups[f"P{p_index}"] = average_burnup
+#     average_burnups = {}
+#     for fuel_vol in fuel_vol_dict.keys():
+#         p_index, _ = split_pz_name(fuel_vol)
+#         average_burnup = 0
+#         for zidx in range(1, Z + 1):
+#             key_construct = f"fuelP{p_index}Z{zidx}"
+#             average_burnup += fuel_vol_dict[key_construct]["corrected_burnup"] / Z
+#         average_burnups[f"P{p_index}"] = average_burnup
 
-    average_burnups_axial = {}
-    for fuel_vol in fuel_vol_dict.keys():
-        _, z_index = split_pz_name(fuel_vol)
-        average_axial_burnup = 0
-        for pidx in range(1, P + 1):
-            key_construct = f"fuelP{pidx}Z{z_index}"
-            average_axial_burnup += fuel_vol_dict[key_construct]["corrected_burnup"]
-        average_burnups_axial[f"Z{z_index}"] = average_axial_burnup
+#     average_burnups_axial = {}
+#     for fuel_vol in fuel_vol_dict.keys():
+#         _, z_index = split_pz_name(fuel_vol)
+#         average_axial_burnup = 0
+#         for pidx in range(1, P + 1):
+#             key_construct = f"fuelP{pidx}Z{z_index}"
+#             average_axial_burnup += fuel_vol_dict[key_construct]["corrected_burnup"]
+#         average_burnups_axial[f"Z{z_index}"] = average_axial_burnup
 
-    # average_burnups_axial
+#     # average_burnups_axial
 
-    # sum([value for key, value in average_burnups.items()])
+#     # sum([value for key, value in average_burnups.items()])
 
-    # sum([value for key, value in average_burnups_axial.items()]) / Z
+#     # sum([value for key, value in average_burnups_axial.items()]) / Z
 
-    # average_burnups
+#     # average_burnups
 
-    df = pd.DataFrame.from_dict(fuel_vol_dict).T
-    sorted_df = df.sort_values(["z", "p"])
-    corrected_burnup_mat = sorted_df["corrected_burnup"].to_numpy().reshape(Z, P)
+#     df = pd.DataFrame.from_dict(fuel_vol_dict).T
+#     sorted_df = df.sort_values(["z", "p"])
+#     corrected_burnup_mat = sorted_df["corrected_burnup"].to_numpy().reshape(Z, P)
 
-    return (sorted_df, corrected_burnup_mat, average_burnups, average_burnups_axial)
+#     return (sorted_df, corrected_burnup_mat, average_burnups, average_burnups_axial)
 
 
 def get_bu_data(
@@ -137,7 +137,7 @@ def get_bu_data(
     indxs = np.array(used_indxs)
 
     materials = dep.materials.keys()
-    # create my own dict with fuel vol's
+
     fuel_vol_dict = {}
 
     # Go through each fuel material volume
@@ -148,7 +148,6 @@ def get_bu_data(
         p_index, z_index = split_pz_name(str(fuel_vol))
         # sum the mdens of each isotope in the fuel volume
         mdens_sum = fuel_data["mdens"][:, -1][indxs].sum()
-        breakpoint()
         serpent_burnup = fuel_data["burnup"][-1]
 
         fuel_vol_dict[fuel_vol] = {}
@@ -176,3 +175,4 @@ def get_bu_data(
     print("BU by Z", sum_burnup_by_z)
     corrected_burnup_mat = df["corrected_burnup"].to_numpy().reshape(Z, P)
     return (df, corrected_burnup_mat, average_burnup, sum_burnup_by_z)
+
