ffa6d4bcbd4312a481f0995573fb751a61b1a4e6
McJackass <alexandru.stafie@gmail.com>
2024-06-18 00:18:38 +0900

    implemented the right algoritm, but there are maybe issues with the reading

--- py_analysis_scripts/get_BU_data/get_bu_data_start_up.py
+++ py_analysis_scripts/get_BU_data/get_bu_data_start_up.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import serpentTools
 import pandas as pd
 from pathlib import Path
-from nuclear_lib.get_bu_data import get_bu_data
+from nuclear_lib.get_bu_data import get_bu_data, get_bu_data2
 import os
 from natsort import natsorted
 from tqdm import tqdm
@@ -26,11 +26,13 @@ Z = 11  # max no of slices
 
 AV = 0.6221408e24
 
-#NOTE: This script uses the nuclear lib
-#NOTE: It needs to be run remote form the base directory
+# NOTE: This script uses the nuclear lib
+# NOTE: It needs to be run remote form the base directory
+
 
 def read_file(file_loc: str) -> DepletionReader:
     return sp.read(file_loc)
+
 
 def main() -> None:
     atomic_wt = pd.read_csv("nuclear_lib/isotope_awt_list.csv")
@@ -43,7 +45,7 @@ def main() -> None:
     simulation.sort(key=lambda x: x.name)
 
     # iterate through all of the simulations and get all paths for one folder at a time
-    for cy_folder in natsorted(simulation):
+    for cy_folder in natsorted(simulation[1:]):
         # reinitialize the parths list
 
         files_in_cy_folder = [
@@ -54,40 +56,54 @@ def main() -> None:
 
         dfs = []
 
-        for file in tqdm(files_in_cy_folder):
+        for file in tqdm(files_in_cy_folder[:50]):
             dep = serpentTools.read((file), reader="dep")
             df, _, _, _ = get_bu_data(dep, atomic_wt, P, Z)
+            df = df[["p", "z", "mdens_sum", "serpent_burnup"]]
+            df = get_bu_data2(dep, atomic_wt, P, Z)
+            df = df.sort_values(["p", "z"]).reset_index(drop=True)
+            # breakpoint()
             dfs.append(df)
-            
 
-        writer = pd.ExcelWriter(BASE_DIR / f"{cy_folder.name}_data.xlsx")
-
-        first_df = dfs[0]
-        first_df = first_df[ first_df['p'] == P]
-        first_df = first_df[["p", "z", "mdens_sum", "serpent_burnup"]]
-        first_df.to_excel(writer, sheet_name=Path(files_in_cy_folder[0]).parent.name)
-
-        original_index = dfs[0].sort_values(['p', 'z']).index
+        # breakpoint()
+        prev_df = dfs[0]
+        corrected_dfs = [prev_df.reset_index(drop=True)]
         for i in range(1, len(dfs)):
-            prev_df = dfs[i-1]
             current_df = dfs[i]
 
             prev_df = prev_df.sort_values(["p", "z"]).reset_index(drop=True)
             current_df = current_df.sort_values(["p", "z"]).reset_index(drop=True)
-            shifted_df = pd.concat([current_df.iloc[Z:], current_df.iloc[:Z]]).reset_index(drop=True)
+            current_shifted_df = current_df
+            current_shifted_df = pd.concat(
+                [current_df.iloc[Z:], current_df.iloc[:Z]]
+            ).reset_index(drop=True)
 
-            shifted_df['serpent_burnup'] = shifted_df['serpent_burnup'] * (shifted_df['mdens_sum'] / prev_df['mdens_sum'])
-            reshifted_df = pd.concat([shifted_df.iloc[-Z:] ,shifted_df.iloc[:-Z]]).reset_index(drop=True)
-            reshifted_df = reshifted_df.set_index(original_index)
-            last_p = reshifted_df[ reshifted_df['p'] == P]
-            last_p = last_p[["p", "z", "mdens_sum", "serpent_burnup"]]
-            last_p.to_excel(writer, sheet_name=Path(files_in_cy_folder[i]).parent.name)
+            current_shifted_df["serpent_burnup"] = prev_df["serpent_burnup"] + (
+                current_shifted_df["serpent_burnup"]
+                * (current_shifted_df["mdens_sum"] / prev_df["mdens_sum"])
+            )
+            current_shifted_df.iloc[-Z:] = current_df.iloc[:Z]
+            reshifted_df = pd.concat(
+                [current_shifted_df.iloc[-Z:], current_shifted_df.iloc[:-Z]]
+            ).reset_index(drop=True)
 
+            prev_df = reshifted_df.copy()
+            corrected_dfs.append(reshifted_df)
+        writer = pd.ExcelWriter(BASE_DIR / f"{cy_folder.name}_data.xlsx")
+        breakpoint()
+
+        means = []
+        for i, df in enumerate(corrected_dfs):
+            df = df[df["p"] == P]
+            mean = df["serpent_burnup"].mean()
+            means.append(mean)
+            df.to_excel(writer, sheet_name=Path(files_in_cy_folder[i]).parent.name)
         writer.save()
+        import numpy as np
 
-
-
-        # writer = pd.ExcelWriter(BASE_DIR / f"{cy_folder.name}_data.xlsx", engine='openpyxl')
+        plt.scatter(np.arange(1, len(means) + 1), means)
+        plt.show()
+        breakpoint()
 
         # current_row = 0
         # space_between = 2
@@ -122,6 +138,6 @@ def main() -> None:
 
         # writer.save()
 
+
 if __name__ == "__main__":
     main()
- 
