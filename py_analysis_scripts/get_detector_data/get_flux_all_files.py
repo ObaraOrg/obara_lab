3e0dc9a6b211e5842e994bc1c2994494bba0c2aa
Alexandru Stafie <alexandru.stafie@gmail.com>
2024-07-03 16:15:56 +0900

    made to get the dpa for all the FA

--- py_analysis_scripts/get_detector_data/get_flux_all_files.py
+++ py_analysis_scripts/get_detector_data/get_flux_all_files.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import serpentTools
 import pandas as pd
 import numpy as np
+import re
 from pathlib import Path
 from nuclear_lib.get_bu_data import get_bu_data, get_bu_data2
 import os
@@ -18,8 +19,8 @@ serpentTools.settings.rc["serpentVersion"] = "2.1.32"
 serpentTools.settings.rc["verbosity"] = "error"
 
 # Set pd display options to show all rows and columns
-pd.set_option("display.max_rows", None)
-pd.set_option("display.max_columns", None)
+# pd.set_option("display.max_rows", None)
+# pd.set_option("display.max_columns", None)
 
 os.environ["CLICK_PAGER"] = "1"
 
@@ -27,10 +28,30 @@ BASE_DIR = Path(os.path.dirname(__file__))
 FILE_NAME = "wh_lfr"
 LAST_DET = "det3"
 
+FLUX_2_DPA = 2.00E+21
+
+# NOTE: Get it from the dep file instead
+TIME = 1050 # 1050 days
+TIME_sec = TIME * 24 * 60 * 60
+
 P = 48  # max no of FA
 Z = 11  # max no of slices
 
-
+def get_no_from_dir(dir_name):
+    """
+    Extracts the trailing number from a directory name.
+    
+    Parameters:
+        directory_name (str): The name of the directory.
+        
+    Returns:
+        str: The extracted number as a string, or None if no number is found.
+    """
+    match = re.search(r'\d+$', dir_name)
+    if match:
+        return match.group()
+    else:
+        return None
 
 
 def main() -> None:
@@ -43,7 +64,9 @@ def main() -> None:
 
     # iterate through all of the simulations and get all paths for one folder at a time
     for cy_folder in natsorted(simulation):
-        # reinitialize the parths list    
+        # reinitialize the paths list    
+
+        flux_data_frame = []
 
         files_in_cy_folder = [
             str(file) for file in sorted(cy_folder.rglob(f"{FILE_NAME}_{LAST_DET}.m"))
@@ -51,19 +74,50 @@ def main() -> None:
         # sort the cy_folders
         files_in_cy_folder = natsorted(files_in_cy_folder)     
 
-        dfs = []
+        det_array = pd.DataFrame(columns=["step_name", "step_no", "p", "z", "flux", "dpa"])
 
+        # read the det file and get the flux values
         for file in tqdm(files_in_cy_folder):
-            breakpoint()
-            dep = serpentTools.read((file), reader="dep")
             det_data = sp.read(file, reader="det")
+            step_name = Path(file).parent.name
+            step_no = get_no_from_dir(step_name)
+
+            det_array = pd.DataFrame(columns=["p", "z", "flux"])
+
+            # Get the flux values for each detector
+            for p in range(1, P+1):
+                flux_values = det_data[f"flux_odsP{p}"].tallies
+                z_values = list(range(1, len(flux_values) + 1))
+                p_values = [p] * len(flux_values)
+                
+                df = pd.DataFrame({
+                    "p": p_values,
+                    "z": z_values,
+                    "flux": flux_values,
+                    "dpa": flux_values*TIME_sec/FLUX_2_DPA,
+
+                })
+
+                det_array = pd.concat([det_array, df], ignore_index=True)
+                            
+            # Add the file name and step to each row of the current det_array
+            det_array["step_name"] = step_name
+            det_array["step_no"] = step_no
+
+
+            # Append the current det_array to the larger flux_data_frame
+            flux_data_frame.append(det_array)
+
+
+        breakpoint()  
+        
 
 
 
-# data_array = []
 
-# for p in range(1, P+1):
-#     data_array.append(det_data.detectors[f"flux_odsP{p}"].tallies)
+
+
+
 
 # df = pd.DataFrame(data_array)
 # print(df)
