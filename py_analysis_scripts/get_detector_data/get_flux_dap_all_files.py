87f3f4919729c17dcc65ce788a32598a52fcc6ca
Alexandru Stafie <alexandru.stafie@gmail.com>
2024-07-03 16:44:39 +0900

    can output cumulative dpa in a excell now

--- py_analysis_scripts/get_detector_data/get_flux_dap_all_files.py
+++ py_analysis_scripts/get_detector_data/get_flux_dap_all_files.py
@@ -28,26 +28,27 @@ BASE_DIR = Path(os.path.dirname(__file__))
 FILE_NAME = "wh_lfr"
 LAST_DET = "det3"
 
-FLUX_2_DPA = 2.00E+21
+FLUX_2_DPA = 2.00e21
 
 # NOTE: Get it from the dep file instead
-TIME = 1050 # 1050 days
+TIME = 1050  # 1050 days
 TIME_sec = TIME * 24 * 60 * 60
 
 P = 48  # max no of FA
 Z = 11  # max no of slices
 
+
 def get_no_from_dir(dir_name):
     """
     Extracts the trailing number from a directory name.
-    
+
     Parameters:
         directory_name (str): The name of the directory.
-        
+
     Returns:
         str: The extracted number as a string, or None if no number is found.
     """
-    match = re.search(r'\d+$', dir_name)
+    match = re.search(r"\d+$", dir_name)
     if match:
         return match.group()
     else:
@@ -64,7 +65,7 @@ def main() -> None:
 
     # iterate through all of the simulations and get all paths for one folder at a time
     for cy_folder in natsorted(simulation):
-        # reinitialize the paths list    
+        # reinitialize the paths list
 
         flux_data_frame = []
 
@@ -72,9 +73,11 @@ def main() -> None:
             str(file) for file in sorted(cy_folder.rglob(f"{FILE_NAME}_{LAST_DET}.m"))
         ]
         # sort the cy_folders
-        files_in_cy_folder = natsorted(files_in_cy_folder)     
+        files_in_cy_folder = natsorted(files_in_cy_folder)
 
-        det_array = pd.DataFrame(columns=["step_name", "step_no", "p", "z", "flux", "dpa"])
+        det_array = pd.DataFrame(
+            columns=["step_name", "step_no", "p", "z", "flux", "dpa"]
+        )
 
         # read the det file and get the flux values
         for file in tqdm(files_in_cy_folder):
@@ -84,40 +87,82 @@ def main() -> None:
 
             det_array = pd.DataFrame(columns=["p", "z", "flux"])
 
-            # Get the flux values for each detector
-            for p in range(1, P+1):
+            # Get the flux values and calculate dpa for each detector
+            for p in range(1, P + 1):
                 flux_values = det_data[f"flux_odsP{p}"].tallies
                 z_values = list(range(1, len(flux_values) + 1))
                 p_values = [p] * len(flux_values)
-                
-                df = pd.DataFrame({
-                    "p": p_values,
-                    "z": z_values,
-                    "flux": flux_values,
-                    "dpa": flux_values*TIME_sec/FLUX_2_DPA,
 
-                })
+                df = pd.DataFrame(
+                    {
+                        "p": p_values,
+                        "z": z_values,
+                        "flux": flux_values,
+                        "dpa": flux_values * TIME_sec / FLUX_2_DPA,
+                    }
+                )
 
                 det_array = pd.concat([det_array, df], ignore_index=True)
-                            
+
             # Add the file name and step to each row of the current det_array
             det_array["step_name"] = step_name
             det_array["step_no"] = step_no
 
-
             # Append the current det_array to the larger flux_data_frame
             flux_data_frame.append(det_array)
 
+            # Same script almost as the get get_bu_data_start_up.py script
 
-        breakpoint()  
+        # breakpoint()
         
+        prev_df = flux_data_frame[0]
+        corrected_dfs = [prev_df.reset_index(drop=True)]
+        
+        for i in range(1, len(flux_data_frame)):
+            current_df = flux_data_frame[i]
 
+            prev_df = prev_df.sort_values(["p", "z"]).reset_index(drop=True)
+            current_shifted_df = current_df.sort_values(["p", "z"]).reset_index(
+                drop=True
+            )
+            current_shifted_df = pd.concat(
+                [current_df.iloc[Z:], current_df.iloc[:Z]]
+            ).reset_index(drop=True)
 
+            current_shifted_df["flux"] = prev_df["flux"] + (
+                current_shifted_df["flux"])
+            current_shifted_df.iloc[-Z:] = current_df.iloc[:Z]
+            reshifted_df = pd.concat(
+                [current_shifted_df.iloc[-Z:], current_shifted_df.iloc[:-Z]]
+            ).reset_index(drop=True)
 
+            prev_df = reshifted_df.copy()
+            corrected_dfs.append(reshifted_df)
 
+        # Combine all corrected DataFrames into a single DataFrame
+        combined_df = pd.concat(corrected_dfs, ignore_index=True)
 
+        # Save the combined DataFrame to a single Excel sheet
+        with pd.ExcelWriter(BASE_DIR / f"{cy_folder.name}_data.xlsx") as writer:
+            combined_df.to_excel(writer, index=False, sheet_name="Sheet1")
 
+        
+        # Collect each Z BU for each discharged fuel assembly (max P)
+        last_fa_bu = []
+        for i, df in enumerate(corrected_dfs):
+            dfs = df[df["p"] == P]
+            last_fa = dfs["flux"].reset_index(drop=True).copy()
+            last_fa.name = str(i)  # Rename the column to the value of i+1
+            last_fa_bu.append(last_fa)
 
+        # Combine all Series into a single DataFrame
+        combined_df = pd.concat(last_fa_bu, axis=1)
+        combined_df.to_csv(BASE_DIR / f"{cy_folder.name}_last_fa_dpa.csv", index=False)
+
+        # plt.scatter(np.arange(1, len(means) + 1), means)
+        # plt.savefig(BASE_DIR / f"{cy_folder.name}_dpa.png")    
+        # breakpoint()  
+        
 
 # df = pd.DataFrame(data_array)
 # print(df)
